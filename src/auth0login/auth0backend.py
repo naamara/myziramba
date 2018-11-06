cfea8a477037093e451bb5147b0dc909ed580ae2
mandela <mandelashaban593@gmail.com>
2018-11-06 23:10:16 +0300

    corrected auth0 error

--- src/auth0login/auth0backend.py
+++ src/auth0login/auth0backend.py
@@ -13,18 +13,18 @@ class Auth0(BaseOAuth2):
     
     def authorization_url(self):
         """Return the authorization endpoint."""
-        return "https://" + self.setting('DOMAIN') + "/authorize"
+        return "https://" + "mandela.auth0.com" + "/authorize"
     
     def access_token_url(self):
         """Return the token endpoint."""
-        return "https://" + self.setting('DOMAIN') + "/oauth/token"
+        return "https://" + "mandela.auth0.com" + "/oauth/token"
     
     def get_user_id(self, details, response):
         """Return current user id."""
         return details['user_id']
     
     def get_user_details(self, response):
-        url = 'https://' + self.setting('DOMAIN') + '/userinfo'
+        url = 'https://' + 'mandela.auth0.com' + '/userinfo'
         headers = {'authorization': 'Bearer ' + response['access_token']}
         resp = requests.get(url, headers=headers)
         userinfo = resp.json()
